d1484a9b554849ed1f19748c93b050c2c13ab161
Jared <jared.michael.wood@gmail.com>
2018-07-31 19:26:37 -0500

    Started project.

---
+++ Capstone_Project/src/main/java/launchcode/com/ftf/FoodTruckFinderApplication.java
@@ -0,0 +1,12 @@
+package launchcode.com.ftf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckFinderApplication.class, args);
+	}
+}
